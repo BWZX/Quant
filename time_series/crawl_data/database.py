9b174640f2f79d7a317c15db1067e2430063128d
adajass <jassada@qq.com>
2017-03-12 12:15:30 +0800

    input data into tsdb

--- time_series/crawl_data/database.py
+++ time_series/crawl_data/database.py
@@ -1,31 +1,55 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from time_series.crawl_data import config
+# from time_series.crawl_data import config
+import config
 import json
 import urllib, urllib.request
 
 
 def insertList(data): 
+    printmsg=data[1]
+    printmsg='code={code}, period={period}, price={price}'.format(code=printmsg['tags'].get('code'),period=printmsg['tags'].get('period'),price=printmsg['tags'].get('price'))
     if not data:   
         return
-    url='http://10.8.0.5:4242/api/put'
-    data = json.dumps(data)  
-    data=bytes(data,'utf8')  
+    url='http://10.8.0.5:4242/api/put'   
+
+    request=urllib.request.Request(url) 
+    index=0
+    length=len(data)
+    loop=True
+    while loop:
+        if length>index+30:
+            tempdt = json.dumps(data[index:index+30])  
+            tempdt=bytes(tempdt,'utf8') 
+            result=urllib.request.urlopen(request, tempdt)
+        else:
+            tempdt = json.dumps(data[index:])
+            # print(tempdt)  
+            tempdt=bytes(tempdt,'utf8') 
+            # print(tempdt)
+            result=urllib.request.urlopen(request, tempdt)
+            loop=False
+        
+        feedback=str(result.getcode())
+        
+        if feedback == '204' or '200':
+            index=index+30
+            print(feedback+' '+printmsg) 
+    pass    
+
+def getPriceData(period, code, price):    
+    url='http://10.8.0.5:4242/q?start=1m-ago&m=none:security.price{period={period},code={code},price={price}'.format(period=period, code=code, price=price)    
     request=urllib.request.Request(url)  
-    result=urllib.request.urlopen(request,data)
+    result=urllib.request.urlopen(request)
     print(result.getcode())  
     #print(data)
     pass
 
-def getData(data): 
-    if not data:   
-        return
-    url='http://10.8.0.5:4242/q?start=1m-ago&m=none:security.price{period={day},code={code},price={price}'
-    data = json.dumps(data)  
-    data=bytes(data,'utf8')  
+def getVolumeData(period, code):    
+    url='http://10.8.0.5:4242/q?start=1m-ago&m=none:security.volume{period={period},code={code}'.format(period=period, code=code)    
     request=urllib.request.Request(url)  
-    result=urllib.request.urlopen(request,data)
+    result=urllib.request.urlopen(request)
     print(result.getcode())  
     #print(data)
     pass
